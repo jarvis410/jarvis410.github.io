71a8105da01262906b20f12d8a0d1a8b430e30a2
parth <parth.makadiya@pmcretail.com>
2020-05-23 19:31:57 +0530

    initial commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+new fullpage('#fullpage', {
+    //Navigation
+    navigation: true,
+    // navigationTooltips: ['About', 'Education', 'Experiences', 'Skills', 'Contact'],
+	// showActiveTooltip: true,
+
+    //Scrolling
+    autoScrolling: true,
+    
+    //Accessibility
+    keyboardScrolling: true,
+    
+    //Design
+})
